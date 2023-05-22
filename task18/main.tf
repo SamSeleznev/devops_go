0ff967c4598998f6c9dbe9bcad0d0944d982859f
SamSeleznev <92021434+SamSeleznev@users.noreply.github.com>
2023-05-22 21:38:51 +0600

    add terra

---
+++ task18/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  //use data aws cli
+}
+
+resource "aws_instance" "ubuntuserver" {
+  ami           = "ami-04cebc8d6c4f297a3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name    = "UbuntuServer"
+    Owner   = "Semen Seleznev"
+    Project = "Task18Terraform"
+  }
+}
+output "instance_ip_addr" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.ubuntuserver.public_ip
+}
